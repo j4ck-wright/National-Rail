d70dfcdc950c3f62f7afb03ecc2dc52806ceaf2a
Jack Wright <jackw221001@gmail.com>
2023-11-19 21:45:56 +0000

    fix: mocking env var

--- src/lib/server/buildXML/buildXML.spec.ts
+++ src/lib/server/buildXML/buildXML.spec.ts
@@ -1,15 +1,23 @@
 import { buildXMLString } from './';
 import { departuresTemplate, serviceDetailsTemplate } from '$lib/server/templates';
 
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, afterEach, vi, beforeEach, beforeAll, afterAll } from 'vitest';
 
 describe('requestBuilder.ts test suite', () => {
+	afterAll(() => {
+		vi.restoreAllMocks();
+	});
+
+	beforeAll(() => {
+		vi.stubEnv('DARWIN_TOKEN', 'token');
+	});
+
 	it('returns correct xml string departure board with mandatory fields', () => {
 		const departureOptions: Darwin.IServiceBoard = {
 			crs: 'XXX'
 		};
 
-		const xmlString = buildXMLString(departuresTemplate, 'token', departureOptions);
+		const xmlString = buildXMLString(departuresTemplate, departureOptions);
 
 		expect(xmlString).toEqual(
 			`<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:typ="http://thalesgroup.com/RTTI/2013-11-28/Token/types" xmlns:ldb="http://thalesgroup.com/RTTI/2017-10-01/ldb/"><soap:Header><typ:AccessToken><typ:TokenValue>token</typ:TokenValue></typ:AccessToken></soap:Header><soap:Body><ldb:GetDepartureBoardRequest><ldb:crs>XXX</ldb:crs></ldb:GetDepartureBoardRequest></soap:Body></soap:Envelope>`
@@ -26,7 +34,7 @@ describe('requestBuilder.ts test suite', () => {
 			timeWindow: 10
 		};
 
-		const xmlString = buildXMLString(departuresTemplate, 'token', departureOptions);
+		const xmlString = buildXMLString(departuresTemplate, departureOptions);
 
 		expect(xmlString).toEqual(
 			`<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:typ="http://thalesgroup.com/RTTI/2013-11-28/Token/types" xmlns:ldb="http://thalesgroup.com/RTTI/2017-10-01/ldb/"><soap:Header><typ:AccessToken><typ:TokenValue>token</typ:TokenValue></typ:AccessToken></soap:Header><soap:Body><ldb:GetDepartureBoardRequest><ldb:numRows>0</ldb:numRows><ldb:crs>XXX</ldb:crs><ldb:filterCrs>YYY</ldb:filterCrs><ldb:filterType>from</ldb:filterType><ldb:timeOffset>5</ldb:timeOffset><ldb:timeWindow>10</ldb:timeWindow></ldb:GetDepartureBoardRequest></soap:Body></soap:Envelope>`
@@ -38,7 +46,7 @@ describe('requestBuilder.ts test suite', () => {
 			serviceID: 'XXX'
 		};
 
-		const xmlString = buildXMLString(serviceDetailsTemplate, 'token', serviceOptions);
+		const xmlString = buildXMLString(serviceDetailsTemplate, serviceOptions);
 
 		expect(xmlString).toEqual(
 			`<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:typ="http://thalesgroup.com/RTTI/2013-11-28/Token/types" xmlns:ldb="http://thalesgroup.com/RTTI/2017-10-01/ldb/"><soap:Header><typ:AccessToken><typ:TokenValue>token</typ:TokenValue></typ:AccessToken></soap:Header><soap:Body><ldb:GetServiceDetailsRequest><ldb:serviceID>XXX</ldb:serviceID></ldb:GetServiceDetailsRequest></soap:Body></soap:Envelope>`
