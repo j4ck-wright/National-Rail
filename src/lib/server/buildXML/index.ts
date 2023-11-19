d976ee711d0e35898b75aac684937c2eed49a7ea
Jack Wright <jackw221001@gmail.com>
2023-11-19 19:16:28 +0000

    refactor: baken in token env var

--- src/lib/server/buildXML/index.ts
+++ src/lib/server/buildXML/index.ts
@@ -1,8 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export function buildXMLString(
 	template: string,
-	token: string,
 	options: Darwin.IServiceDetails | Darwin.IServiceBoard
 ) {
+	const token = process.env.DARWIN_TOKEN;
+	if (!token) throw Error('You do not have DARWIN_TOKEN as an environment variable');
 	let output = template.replace('!!TokenValue!!', token);
 
 	for (const [key, value] of Object.entries(options)) {
